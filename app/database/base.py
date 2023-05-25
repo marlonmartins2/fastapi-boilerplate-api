cb526125682716e35ca59ea085cfd66b28aed6ee
Marlon Martins <marlon.azevedo.m@gmail.com>
2023-05-24 21:04:02 -0300

    feat: add database module.

---
+++ app/database/base.py
@@ -0,0 +1,36 @@
+from pymongo import MongoClient
+
+from app.settings import settings
+
+
+class Indices(type):
+    """
+    Cria indexs para todas as coleções que herdam essa classe
+    """
+
+    def __init__(cls, *args, **kwargs):
+        if hasattr(cls, "indices"):
+            cls.get_instancia().create_indexes(cls.indices)
+
+        super().__init__(*args, **kwargs)
+
+
+class BaseConexao:
+    if settings.MONGO_SSL is True:
+        conexao = MongoClient(
+            settings.MONGO_URL,
+            tls=True,
+            tlsCAFile=settings.PATH_CERT,
+            tlsAllowInvalidHostnames=True,
+            retryWrites=False,
+            directConnection=True,
+        )
+    else:
+        conexao = MongoClient(settings.MONGO_URL)
+
+
+class BaseDB(BaseConexao, metaclass=Indices):
+    database = settings.DATABASE_ENVIRONMENT
+
+
+db = BaseDB.conexao[BaseDB.database]
